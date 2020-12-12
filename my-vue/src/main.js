78ee63ccae332d63cb459a485e3eed96b650e61c
zhaiyunfan20000324@qq.com <30565458+zhaiyunfan@users.noreply.github.com>
2020-12-12 17:51:20 +0800

    update

--- my-vue/src/main.js
+++ my-vue/src/main.js
@@ -17,8 +17,8 @@ Vue.use(VueResource)
 // 全局vue添加axios
 Vue.prototype.axios = axios;
 // 设置默认请求路径
-axios.defaults.baseURL = "wr3ck97y.xiaomy.net:30244/springbootdemo/";
-//axios.defaults.baseURL = "http://127.0.0.1:8088/springbootdemo/";
+//axios.defaults.baseURL = "wr3ck97y.xiaomy.net:30244/springbootdemo/";
+axios.defaults.baseURL = "http://127.0.0.1:8088/springbootdemo/";
 // // 拦截器要求axios返回的数据是res.data
 // axios.interceptors.response.use(
 //     // config => {
